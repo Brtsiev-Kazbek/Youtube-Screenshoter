aebd44b24ace8b46c1268c11fcb55ce44f4a65e6
Kazbek <brtsievkazbek@gmail.com>
2021-01-19 14:32:35 +0300

    bugfix

--- src/capture.js
+++ src/capture.js
@@ -2,16 +2,24 @@ import {saveDownload} from './download.js'
 import {createFilename} from './fileName.js'
 
 export const capture = () => {
-    let root = document.querySelector('#primary.ytd-watch-flexy')
-    let uploadInfo = root.querySelector('#upload-info')
+    let root = document.querySelector('#primary.style-scope.ytd-watch-flexy')
+    let uploadInfo = root?.querySelector('#upload-info')
     let video = document.querySelector('video');
     let canvas = document.createElement('canvas')
     let ctx = canvas.getContext('2d')
-    let title = root.querySelector('.title.style-scope') ? document.querySelector('.title.style-scope')?.innerText : document.querySelector('.ytp-title-link.yt-uix-sessionlink')?.innerText
+    let title = root?.querySelector('.title.style-scope') ? document.querySelector('.title.style-scope')?.innerText : document.querySelector('.ytp-title-link.yt-uix-sessionlink')?.innerText
     let videoCurrentTime = video.currentTime
-    let channel = uploadInfo.querySelector('.yt-simple-endpoint.yt-formatted-string') ? uploadInfo.querySelector('.yt-simple-endpoint.yt-formatted-string').innerHTML : document.querySelector('.ytp-ce-channel-title.ytp-ce-link')?.innerHTML;
+    let channelBlock = root?.querySelector('.style-scope.ytd-video-secondary-info-renderer')?.querySelector('#upload-info')
+    let channel = channelBlock?.querySelector('.yt-simple-endpoint.yt-formatted-string') ? channelBlock?.querySelector('.yt-simple-endpoint.yt-formatted-string').innerHTML : document.querySelector('.ytp-ce-channel-title.ytp-ce-link')?.innerHTML;
+    if(!channel) {
+      channel = "[Миниатюра]"
+    }
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
+
+  console.log(root)
+  console.log(uploadInfo)
+  console.log(channel)
 
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
