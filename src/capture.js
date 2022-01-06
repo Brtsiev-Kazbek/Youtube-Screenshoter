6227a3316e3c9ded1637ea5fc71b7f45fac6f08b
Neyasbltb <neyasbltb_88@mail.ru>
2022-01-07 00:18:25 +0300

    captureOnKeys вынесена в отдельный файл и сделана универсальной, она теперь может вызывать любой коллбек. В индексном файле обновлена инициализация

--- src/capture.js
+++ src/capture.js
@@ -2,7 +2,7 @@ import { saveDownload } from './download.js';
 import { createFilename } from './fileName.js';
 import { getVideoInfo } from './getVideoInfo.js';
 
-export const capture = () => {
+const capture = () => {
     let video = document.querySelector('video');
     let canvas = document.createElement('canvas');
     let ctx = canvas.getContext('2d');
@@ -19,24 +19,4 @@ export const capture = () => {
     }, 'image/png');
 };
 
-export const captureOnKeys = (...codes) => {
-    let pressed = new Set();
-
-    document.addEventListener('keydown', function (event) {
-        pressed.add(event.code);
-
-        for (let code of codes) {
-            if (!pressed.has(code)) {
-                return;
-            }
-        }
-
-        pressed.clear();
-
-        capture();
-    });
-
-    document.addEventListener('keyup', function (event) {
-        pressed.delete(event.code);
-    });
-};
+export { capture };
